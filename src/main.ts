370268604e99d1f24a61b2327a6b021ae6242bfa
Danila <danil2005232@gmail.com>
2023-02-17 20:52:00 +0300

    Added CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ["http://localhost:8080"],
+    credentials: true,
+    methods: ["POST"]
+  });
   app.use(cookieParser());
   await app.listen(3000);
 }
